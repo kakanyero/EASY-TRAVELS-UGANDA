508800ec7ef11fad0f1a329b3baca582e4eed24e
kakanyero <kakanyero142@gmail.com>
2023-09-23 13:55:53 +0300

    added onStart in the log in activity

--- app/src/main/java/com/example/easytravels/loginandsignup/LoginActivity.kt
+++ app/src/main/java/com/example/easytravels/loginandsignup/LoginActivity.kt
@@ -57,9 +57,13 @@ class LoginActivity : AppCompatActivity() {
         }
 
 
+    }
+    override fun onStart() {
+        super.onStart()
 
-
-
-
+        if(firebaseAuth.currentUser != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
